ae210b614bc6e16bc2045cd95dfc5a93bc21e83d
monster anshu <hgunwant2312@gmail.com>
2022-07-10 13:56:48 +0530

    v1.0

--- src/Pages/Messenger/Messenger.tsx
+++ src/Pages/Messenger/Messenger.tsx
@@ -16,6 +16,7 @@ const Messenger = () => {
   const { fetchAxios, socket } = useContext(Context);
   const [selected, setSelected] = useState<string | null>(null);
   const [users, setUsers] = useState<user[]>([]);
+  const [selectedUser, setSelectedUser] = useState<user>();
   const [onlineUsers, setOnlineUsers] = useState<
     {
       userId: string;
@@ -27,30 +28,38 @@ const Messenger = () => {
     setSelected(id);
   };
 
-  const selectedUser = users.filter((x) => x['_id'] === selected)[0];
-
-  const fetchUsers = () => {
-    fetchAxios({
-      url: '/chat/getUser/',
-    })
-      .then((res) => {
-        setUsers([]);
-        setUsers(res.data);
-      })
-      .catch((err) => console.log(err));
-  };
-
-  useEffect(() => {
-    fetchUsers();
-
+  const addUser = async () => {
     if (!location.state) return;
     const cond = typeof location.state === 'object';
     if (!cond) return;
     const nUser = location.state as UserType;
     const conds = users.some((user) => user._id === nUser._id);
     if (!conds) setUsers([nUser, ...users]);
+    console.log(nUser);
     setSelected(nUser._id);
+  };
+
+  const fetchUsers = async () => {
+    fetchAxios({
+      url: '/chat/getUser/',
+    })
+      .then((res) => {
+        setUsers([...res.data, ...users]);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  useEffect(() => {
+    const user = users.find((x) => x['_id'] === selected);
+    setSelectedUser(user);
+  }, [selected]);
+
+  useEffect(() => {
+    fetchUsers();
   }, []);
+  useEffect(() => {
+    addUser();
+  }, [users]);
 
   useEffect(() => {
     if (!socket) return;
@@ -62,7 +71,7 @@ const Messenger = () => {
   return (
     <Container>
       <ChatsMenu selected={selected} onSelect={handelSelect} users={users} />
-      {selected ? (
+      {selectedUser ? (
         <ChatContainer
           name={selectedUser['name']}
           img={selectedUser['profilePicture']}
